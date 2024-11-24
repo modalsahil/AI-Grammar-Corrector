adf1759be20032d44b79f8476a8f53d07725a687
modalsahil <134227557+modalsahil@users.noreply.github.com>
2024-11-23 17:16:27 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+/** Created by Sahil Sahota
+ * 
+ * A program originally designed to help my father 
+ * fix his grammar when wiritng texts and emails.
+ * 
+ * All code is written by Sahil Sahota.
+ * 
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        new GrammarAppGUI();
+        //System.out.println("help");
+    }
+}
